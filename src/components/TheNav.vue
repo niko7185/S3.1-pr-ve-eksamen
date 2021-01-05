de2c8d64dd4dfb227677ec6367f73cfc46d17ca8
Nikolaj Schmidt Christoffersen <niko7185@edu.campusvejle.dk>
2021-01-05 10:45:25 +0100

    Added a header and nav buttons for authorization

---
+++ src/components/TheNav.vue
@@ -0,0 +1,36 @@
+
+<template>
+    <nav>
+      <router-link to="/about">Log In</router-link>
+      <router-link to="/about">Register</router-link>
+    </nav>
+</template>
+
+<style scoped>
+    nav {
+        max-width: 100%;
+        margin: auto;
+        padding: 2px 0;
+        text-align: center;
+    }
+
+    a {
+        color: white;
+        padding: 0.5rem 1rem;
+        text-decoration: none;
+        display: inline-block;
+    }
+
+    a:hover {
+        background-color:#e7e7e7;
+        color: darkgreen;
+    }
+    
+    @media screen and (min-width: 250px) {
+        nav {
+            text-align: left;
+            max-width: 70%;
+        }
+    }
+
+</style>
